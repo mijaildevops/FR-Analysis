93b2c9d2197f7653ed6cd1356ba9b27ddb505865
Mijail <mijail.devops@gmail.com>
2020-07-03 21:52:38 -0500

    Link page Reset Code

---
+++ js/ResetCode.js
@@ -0,0 +1,52 @@
+console.log("ResetCode .js");
+
+const form = document.getElementById('ResetCode');
+const user = document.getElementById('email');
+
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    let users = Array ({
+        usuario: user.value,
+       
+    });
+    console.log(users);
+    //localStorage.setItem('User',JSON.stringify(users));
+    //location.href='Api-data.html';
+    
+    var formData = new FormData();
+    var Peticion = 2
+    var Email = users[0].usuario
+    
+    
+    formData.append('Email', Email);
+    formData.append('Peticion', Peticion);
+
+    fetch('http://100.97.218.207:5080/Code', {
+    method: 'PUT',
+    body: formData
+    })
+        .then(Res=>Res.json())
+        .then(Res=>{
+        //console.log(Res)
+        let MError = Res.Error
+        console.log(MError)
+
+        if (MError === undefined) {
+  
+            Message.innerHTML = '';
+            Message.innerHTML += `
+            <p style="color:green">${Res.Mensaje}</p>
+          `
+            
+        }else{
+            
+            Message.innerHTML = '';
+            Message.innerHTML += `
+            <p style="color:red">${Res.Error}</p>
+          `
+        }
+        
+
+    
+      })})
